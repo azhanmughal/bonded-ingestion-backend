8b727bdf23b590537b83cbcfba3d3aab91195e32
azhanmughal <azhanmughal00@gmail.com>
2025-05-30 16:21:09 +0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from ingestion_router import router
 
-app = FastAPI()
+app = FastAPI(
+    title="Bonded Ingestion API",
+    docs_url="/docs"
+)
+
+@app.get("/")
+def root():
+    return {"message": "Backend is working"}
+
+
 app.include_router(router)
